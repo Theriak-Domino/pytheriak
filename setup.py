3b692e7276847a1394194bcdd08e78f4e0563023
Philip Hartmeier <philip.hartmeier@hotmail.com>
2023-02-10 18:28:03 +0100

    changed pkg structure

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup
-from setuptools import find_packages
 
 with open("README.md", "r") as rm:
     long_description = rm.read()
 
 setup(
     name="pytheriak",
-    version="0.0.4",
+    version="0.0.7",
     description="Wrappers to call and read_out theriak from python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    py_modules=["theriak"],
-    # package_dir={"": "src"},
-    packages=["wrapper"],
+    py_modules=["pytheriak.wrapper"],
+    package_dir={"": "src"},
     python_requires=">=3.10",
     install_requires=["numpy >= 1.23", ],
     classifiers=[
