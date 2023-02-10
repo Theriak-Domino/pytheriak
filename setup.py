dfc18e1d98b3bc059b677989f7d290cd483b207a
Philip Hartmeier <philip.hartmeier@hotmail.com>
2023-02-10 18:00:43 +0100

    Chnaged pkg structure

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
+from setuptools import find_packages
 
 with open("README.md", "r") as rm:
     long_description = rm.read()
 
 setup(
     name="pytheriak",
-    version="0.0.2",
+    version="0.0.4",
     description="Wrappers to call and read_out theriak from python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     py_modules=["theriak"],
-    package_dir={"": "src"},
+    # package_dir={"": "src"},
+    packages=["wrapper"],
     python_requires=">=3.10",
     install_requires=["numpy >= 1.23", ],
     classifiers=[
