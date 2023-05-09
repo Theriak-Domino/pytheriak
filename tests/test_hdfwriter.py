fc8268f08a3fb8827f93771db02f16710616a6b7
Philip Hartmeier <philip.hartmeier@hotmail.com>
2023-05-09 10:45:22 +0200

    buf ifx: missing "_"

--- tests/test_hdfwriter.py
+++ tests/test_hdfwriter.py
@@ -73,7 +73,7 @@ def test_bulkcomposition(hdf_file, rock_group_names):
 def test_dG_metasatbale_minerals(hdf_file, rock_group_names):
     BENCHMARK_dG_ST_fst_rock1 = 3.01992E+03
 
-    list_metastable_minerals_rock1 = hdf_file[rock_group_names[0]]["delta_G_meta-stable_minerals"].attrs["mineral names"]
+    list_metastable_minerals_rock1 = hdf_file[rock_group_names[0]]["delta_G_meta-stable_minerals"].attrs["mineral_names"]
     idx_ST_fst_rock1 = np.where(list_metastable_minerals_rock1 == "ST_fst")[0]
 
     dG_ST_fst_rock1 = hdf_file[rock_group_names[0]]["delta_G_meta-stable_minerals"][idx_ST_fst_rock1][0]
